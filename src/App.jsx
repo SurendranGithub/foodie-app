3ef542905c8dd45fdaebbb4cfe87f9d756e5943e
SurendranGithub <prasannarajan2107@gmail.com>
2025-05-21 12:10:55 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-center text-3xl font-bold text-blue-500'>
+      Hello !
+    </div>
+  )
+}
+
+export default App
